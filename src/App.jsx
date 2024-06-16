f845b463b23142096bc7f13cc0040418aa13cce9
Elmo Tirado <etirado@unitru.edu.pe>
2024-06-16 13:25:23 -0500

    funciona

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
+import Contactarnos from './vistas/contactarnos'
 
 function App() {
 
   return (
     <>
-      <div className='text-green-500'>
-        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique voluptatibus perspiciatis reiciendis libero omnis excepturi tenetur rerum, molestiae, nihil laboriosam unde quos magni illum at expedita. Excepturi inventore doloribus ad?</h1>
-      </div>
-      <img src="./img/bistec.jpg" alt="" />
+      <Contactarnos />
     </>
   )
 }
