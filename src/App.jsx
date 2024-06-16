4e6014f85a9e1c3c2865354db7e33e44681a3d95
Elmo Tirado <etirado@unitru.edu.pe>
2024-06-16 13:11:40 -0500

    firts commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-green-500'>
+        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique voluptatibus perspiciatis reiciendis libero omnis excepturi tenetur rerum, molestiae, nihil laboriosam unde quos magni illum at expedita. Excepturi inventore doloribus ad?</h1>
+      </div>
+      <img src="./img/bistec.jpg" alt="" />
+    </>
+  )
+}
+
+export default App
